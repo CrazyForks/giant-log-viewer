3ff1f2c866b3bf6348169722371cc04db8ce80c2
Sunny Chung <sunnychung@live.hk>
2025-04-13 16:10:07 +0800

    add dark color palette

--- src/jvmMain/kotlin/com/sunnychung/application/multiplatform/giantlogviewer/document/ColorTheme.kt
+++ src/jvmMain/kotlin/com/sunnychung/application/multiplatform/giantlogviewer/document/ColorTheme.kt
@@ -73,3 +73,44 @@ fun lightColorTheme(): ColorTheme = ColorTheme(
     dialogBackground = Color.Cyan,
     dialogPrimary = Color(red = 0f, green = 0f, blue = 0.19f),
 )
+
+fun darkColorTheme(): ColorTheme {
+    val primaryBackground = Color(.06f, .06f, .06f)
+    val brightBackground = Color(0.235f, 0.196f, 0.196f)
+    val primaryText = Color(0.722f, 0.682f, 0.682f)
+    val brightText = Color(0.816f, 0.780f, 0.780f)
+    val disabledText = Color(0.176f, 0.216f, 0.216f)
+    return ColorTheme(
+        name = "Dark",
+        background = Color(.4f, .4f, .4f),
+        bigTextHint = Color.Gray,
+        bigTextWarning = Color(red = 0.7f, green = 0.4f, blue = 0.4f),
+        textFieldPlaceholder = Color.Gray,
+        textFieldBackground = brightBackground,
+        textFieldCursor = brightText,
+        textFieldContent = brightText,
+        contextMenuBackground = Color(0.071f, 0.071f, 0.071f),
+        contextMenuText = brightText,
+        contextMenuDisabledText = disabledText,
+        contextMenuDividerLine = Color.Gray,
+        menuBarBackground = Color(red = 0.3f, green = 0.3f, blue = 0.3f),
+        menuBarIconColor = Color.White,
+        menuBarTextColor = Color.White,
+        toggleButtonOnText = primaryText,
+        toggleButtonDisabledText = Color(.4f, .4f, .4f),
+        toggleButtonOnBackground = Color(.2f, .2f, .7f),
+        toggleButtonOffBackground = Color(0f, 0f, .3f),
+        readPositionBarNegativeBackground = Color(red = 0.19f, green = 0.19f, blue = 0.19f),
+        readPositionBarPositiveBackground = Color(red = 0.85f, green = 0.6f, blue = 0f),
+        readPositionBarText = Color.White,
+        fileBodyTheme = FileBodyTheme(
+            searchMatchBackground = Color(0.451f, 0.373f, 0.000f),
+            selectionBackground = Color(red = 0.2f, green = 0.2f, blue = 0.8f, alpha = .6f),
+            plainText = primaryText,
+            background = primaryBackground,
+        ),
+        aboutDialogText = primaryText,
+        dialogBackground = primaryBackground,
+        dialogPrimary = Color(0.486f, 0.780f, 1.000f),
+    )
+}
